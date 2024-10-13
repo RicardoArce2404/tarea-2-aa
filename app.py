c5ee6e9d4c7f919efdc09f6e2fe6b8535e9ec62b
okRoni <oscar.gt.140.ro@gmail.com>
2024-10-13 01:59:18 -0600

    fix asynchronous process

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from random import randint
-import time
+import eventlet # eventlet is an asynchronous framework that works with Flask-SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='eventlet')
@@ -10,11 +10,14 @@ socketio = SocketIO(app, async_mode='eventlet')
 def index():
     return render_template('index.html')
 
+# this is a test that prints a random number every second
+# to the console and emits it to the client
+# the client will display the number in the console as well
 @socketio.on('start_test')
 def start_test():
     print('Test started')
     for i in range(10):
-        time.sleep(1)
+        eventlet.sleep(1) # IMPORTANT! we use eventlet.sleep instead of time.sleep!!
         random_number = randint(1, 100)
         print('Random number:', random_number)
         socketio.emit('new_number', random_number)
