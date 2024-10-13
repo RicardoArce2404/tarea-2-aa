0648f741593f805fb663092eb03d8a56b13d4c43
okRoni <oscar.gt.140.ro@gmail.com>
2024-10-13 01:50:37 -0600

    fix project not initializing

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from random import randint
+import time
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
 
 @app.route('/')
 def index():
@@ -14,9 +14,10 @@ def index():
 def start_test():
     print('Test started')
     for i in range(10):
+        time.sleep(1)
         random_number = randint(1, 100)
         print('Random number:', random_number)
         socketio.emit('new_number', random_number)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
